33727c9a5edeebcfe7a8d8f882a5c0d34863f2b0
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-06-26 16:33:50 +0200

    chore: Update settings.py to include frontend/dist directory for templates

--- backend/settings.py
+++ backend/settings.py
@@ -106,8 +106,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'dist'),
     os.path.join(BASE_DIR, 'frontend', 'dist','assets'),
+    os.path.join(BASE_DIR, 'frontend', 'dist'),
+    
 ]
 
 CLOUDINARY_STORAGE = {
