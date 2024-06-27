1e97e0bf08b1b702a648b668d90b191a6b558bd2
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-06-27 11:20:02 +0200

    chore: Update base.html as the default template for the home page

--- backend/urls.py
+++ backend/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='base.html')),
     path('api/users/', include('users.urls')),
     path('api/profiles/', include('profiles.urls')),
     path('api/blog/', include('blog.urls')),
