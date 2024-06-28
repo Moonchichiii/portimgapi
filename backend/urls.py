fe9370ee01517e338cd9be347f279a71bb3e003d
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-06-28 14:09:56 +0200

    chore: Update Django settings configuration

--- backend/urls.py
+++ backend/urls.py
@@ -4,15 +4,17 @@ from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='base.html')),
+    path('', TemplateView.as_view(template_name='main.html')),
     path('api/users/', include('users.urls')),
     path('api/profiles/', include('profiles.urls')),
     path('api/blog/', include('blog.urls')),
     path('api/notifications/', include('notifications.urls')),
     path('api/chatbot/', include('chatbot.urls')),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
